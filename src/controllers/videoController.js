6d334eb611570077da59326a4d943874202c1522
rlaclghks123 <55423198+rlaclghks123@users.noreply.github.com>
2021-11-08 22:49:40 +0900

    scss

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,7 +7,7 @@ import Video from "../models/Video";
   
 
 export const home = async(req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).populate("owner");
   return res.render("home", {pageTitle:"Home",videos});
 }
 
