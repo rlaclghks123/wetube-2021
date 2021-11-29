e30c6d4abc201e56338c5948c7313d1bc2a0e297
rlaclghks123 <55423198+rlaclghks123@users.noreply.github.com>
2021-11-29 17:34:51 +0900

    real last modify aws

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -70,6 +70,7 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload" });
 }
 
+const isHeroku = process.env.NODE_ENV === "production";
 
 export const postUpload = async (req, res) => {
   const { user: { _id }, } = req.session
@@ -78,8 +79,8 @@ export const postUpload = async (req, res) => {
   try {
     const newVideo = await Video.create({
       owner: _id,
-      fileUrl: req.session.heroku ? video[0].location : video[0].path,
-      thumbUrl: req.session.heroku ? thumb[0].location : thumb[0].path,
+      fileUrl: isHeroku ? video[0].location : video[0].path,
+      thumbUrl: isHeroku ? thumb[0].location : thumb[0].path,
       title,
       description,
       hashtags: Video.formatHashtag(hashtags)
