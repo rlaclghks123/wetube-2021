8c0c6237a7a35993158d282f89c2e94096725cea
Chi Hwan Kim <rlaclghks123@naver.com>
2021-12-01 16:24:29 +0900

    fixed isHeroku

--- src/middleware.js
+++ src/middleware.js
@@ -15,6 +15,7 @@ const s3ImageUploader = multerS3({
     s3: s3,
     bucket: "wetubechiman/images",
     acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 
@@ -23,6 +24,7 @@ const s3VideoUploader = multerS3({
     s3: s3,
     bucket: "wetubechiman/videos",
     acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 export const localsMiddleware = (req, res, next) => {
