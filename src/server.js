bd02b9d8dfa981731b58ec78b3c2c8258f9937c7
Chi Hwan Kim <rlaclghks123@naver.com>
2021-12-01 17:19:46 +0900

    ha.....

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(session({
 }));
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Cross-Origin-Opener-Policy", "cross-origin");
   next();
 });
 app.use(flash());
