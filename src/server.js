2d057686896fc12f2d32c9bbdd8a1209e0d3477b
Chi Hwan Kim <rlaclghks123@naver.com>
2021-12-01 17:28:19 +0900

    try to fix CORS

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(session({
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
-  res.header("Cross-Origin-Resource-Policy", "cross-origin ")
+  res.header("Cross-Origin-Resource-Policy", "same-site")
   next();
 });
 app.use(flash());
